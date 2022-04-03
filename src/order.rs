484859bf75108cb66e96a42e5c6bfb263fe39e5f
jojii <jojii@gmx.net>
2022-04-03 19:00:06 +0200

    impl more traits

--- src/order.rs
+++ src/order.rs
@@ -1,4 +1,8 @@
-use std::cmp::Ordering;
+use std::{
+    borrow::Borrow,
+    cmp::Ordering,
+    ops::{AddAssign, DivAssign, MulAssign, SubAssign},
+};
 
 /// Custom struct to be able to compare items using another types `Ord` implementation
 pub struct OrderVal<T, O: Ord> {
@@ -19,10 +23,22 @@ impl<T, O: Ord> OrderVal<T, O> {
         self.val
     }
 
+    /// Borrow inner value
+    #[inline]
+    pub fn inner(&self) -> &T {
+        &self.val
+    }
+
     /// Get assigned score
     #[inline]
     pub fn ord(&self) -> &O {
         &self.ord
+    }
+
+    /// Sets the order
+    #[inline]
+    pub fn set_ord(&mut self, ord: O) {
+        self.ord = ord;
     }
 }
 
@@ -60,3 +76,45 @@ impl<T: Clone, O: Ord + Clone> Clone for OrderVal<T, O> {
 }
 
 impl<T: Copy, O: Ord + Copy> Copy for OrderVal<T, O> {}
+
+impl<T, O: Ord> Borrow<T> for OrderVal<T, O> {
+    #[inline]
+    fn borrow(&self) -> &T {
+        self.inner()
+    }
+}
+
+impl<T, O: Ord> AsRef<T> for OrderVal<T, O> {
+    #[inline]
+    fn as_ref(&self) -> &T {
+        self.inner()
+    }
+}
+
+impl<T, O: Ord + AddAssign<O>> AddAssign<O> for OrderVal<T, O> {
+    #[inline]
+    fn add_assign(&mut self, rhs: O) {
+        self.ord.add_assign(rhs)
+    }
+}
+
+impl<T, O: Ord + SubAssign<O>> SubAssign<O> for OrderVal<T, O> {
+    #[inline]
+    fn sub_assign(&mut self, rhs: O) {
+        self.ord.sub_assign(rhs)
+    }
+}
+
+impl<T, O: Ord + DivAssign<O>> DivAssign<O> for OrderVal<T, O> {
+    #[inline]
+    fn div_assign(&mut self, rhs: O) {
+        self.ord.div_assign(rhs)
+    }
+}
+
+impl<T, O: Ord + MulAssign<O>> MulAssign<O> for OrderVal<T, O> {
+    #[inline]
+    fn mul_assign(&mut self, rhs: O) {
+        self.ord.mul_assign(rhs)
+    }
+}
